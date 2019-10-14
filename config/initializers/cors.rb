4eb0e37e2db7e097ec61853a8aa6f37b97cf7fe4
github username <jesse.ben.berman@gmail.com>
2019-10-14 12:53:34 -0400

    adding origin fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://takeouttruckstop.herokuapp.com/' # your client's domain
+    origins '*' # your client's domain
     resource '*',
    
     headers: :any,

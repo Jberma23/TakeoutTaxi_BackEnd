0979fff7e6e52a610c4bc07d67b744b1eade33c4
github username <jesse.ben.berman@gmail.com>
2019-10-16 12:55:24 -0400

    fix host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,16 @@
 class ApplicationController < ActionController::API
   include ::ActionController::Cookies
     before_action :configure_permitted_parameters, if: :devise_controller?
-    
+    protect_from_forgery with: :exception
+    after_action :cors_set_access_control_headers
 
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Max-Age'] = '1728000'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
     def secret_key
       'Richardm'
     end

7b43a17d567e19dcbcfedd9d04d49f30689a6458
github username <jesse.ben.berman@gmail.com>
2019-10-16 11:51:38 -0400

    fix host

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module TakeoutTaxi
 
     # Dotenv::Railtie.load
 
-      HOSTNAME = ENV['https://takeouttruckstop.herokuapp.com']
+      HOSTNAME = ENV['HOSTNAME']
  
     config.to_prepare do
       DeviseController.respond_to :html, :json

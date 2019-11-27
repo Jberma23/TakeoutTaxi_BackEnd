9ac0a9e2be98fe6b6b6286429f8f8ecdeb1eaca8
github username <jesse.ben.berman@gmail.com>
2019-11-27 11:01:56 -0500

    adding cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,3 +4,12 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
+config.middleware.insert_before 0, Rack::Cors do
+    allow do
+        origins 'http://www.takeouttruckstop.com' # your client's domain https://takeouttruckstop.herokuapp.com
+        resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head], 
+        credentials: true
+      end
+    end
